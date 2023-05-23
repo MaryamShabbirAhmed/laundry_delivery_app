45815ed919601ebb7327e266ab08d9dfe4a86d05
maryam shabbir <maryamshabbirahmed@gmil.com>
2023-05-23 22:47:06 +0500

    pickup cloth 0.0.1

---
+++ lib/providers/pickupProvider.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class PickupProvider extends ChangeNotifier{
+
+  TextEditingController pickupCustomerNameController=TextEditingController();
+  TextEditingController pickupLocationController2=TextEditingController();
+  TextEditingController pickupLocationController=TextEditingController();
+  TextEditingController pickupContactController=TextEditingController();
+  TextEditingController pickupRemarksController=TextEditingController();
+  TextEditingController pickupPacketController=TextEditingController();
+  TextEditingController pickupDeliveryDateController=TextEditingController();
+  TextEditingController pickupDeliveryTimeController=TextEditingController();
+
+
+
+}
--- lib/utils/providerVeriables.dart
+++ lib/utils/providerVeriables.dart
@@ -2,5 +2,7 @@ import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/dashboardProvider.dart';
+import '../providers/pickupProvider.dart';
 
 DashboardProvider dashboardPro = Provider.of(Get.context!, listen:false);
+PickupProvider pickupPro = Provider.of(Get.context!, listen:false);
